a5a8c803fb367b64a5b13c8d38582753e8f565fd
denzel84 <baderou@yahoo.fr>
2024-01-28 20:18:02 +0100

    sorting project

---
+++ sort.h
@@ -0,0 +1,45 @@
+#ifndef __SORT__H
+#define __SORT__H
+
+#include<stddef.h>
+
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+
+void print_list(const listint_t *);
+void print_array(const int *, size_t);
+void bubble_sort(int *, size_t);
+void swap_ints(int *a, int *b);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
